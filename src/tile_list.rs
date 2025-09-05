066579cd008138fdbef8b440ae0f40aa4ff825bf
Keitaroh Kobayashi <keita@kbys.me>
2025-09-06 07:36:13 +0900

    Add region filtering by bbox
    Closes #2

--- src/tile_list.rs
+++ src/tile_list.rs
@@ -1,6 +1,7 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    str::FromStr,
 };
 
 use crate::{tile::Tile, tile_list_format::compile_tile_format};
@@ -90,5 +91,62 @@ impl TileList {
     pub fn filter_zooms(&mut self, min: u8, max: u8) {
         self.tiles.retain(|tile| tile.z() >= min && tile.z() <= max);
         self.meta = TileListMeta::new(min, max, &self.tiles);
+    }
+
+    pub fn filter_bbox(&mut self, bbox: SimpleBBox) {
+        self.tiles.retain(|tile| {
+            let (tile_min_lon, tile_min_lat, tile_max_lon, tile_max_lat) = tile.bounds();
+            !(tile_max_lon < bbox.min_x()
+                || tile_min_lon > bbox.max_x()
+                || tile_max_lat < bbox.min_y()
+                || tile_min_lat > bbox.max_y())
+        });
+        if let Some((min_lon, min_lat, max_lon, max_lat)) = self.meta.bounds {
+            let new_min_lon = min_lon.max(bbox.min_x());
+            let new_min_lat = min_lat.max(bbox.min_y());
+            let new_max_lon = max_lon.min(bbox.max_x());
+            let new_max_lat = max_lat.min(bbox.max_y());
+            self.meta.bounds = Some((new_min_lon, new_min_lat, new_max_lon, new_max_lat));
+            self.meta.center = Some((
+                (new_min_lon + new_max_lon) / 2.0,
+                (new_min_lat + new_max_lat) / 2.0,
+            ));
+        }
+    }
+}
+
+pub struct SimpleBBox(f32, f32, f32, f32);
+
+impl SimpleBBox {
+    pub fn min_x(&self) -> f32 {
+        self.0
+    }
+
+    pub fn min_y(&self) -> f32 {
+        self.1
+    }
+
+    pub fn max_x(&self) -> f32 {
+        self.2
+    }
+
+    pub fn max_y(&self) -> f32 {
+        self.3
+    }
+}
+
+impl FromStr for SimpleBBox {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 4 {
+            anyhow::bail!("Invalid bbox format. Expected format: min_x,min_y,max_x,max_y");
+        }
+        let min_x: f32 = parts[0].parse()?;
+        let min_y: f32 = parts[1].parse()?;
+        let max_x: f32 = parts[2].parse()?;
+        let max_y: f32 = parts[3].parse()?;
+        Ok(SimpleBBox(min_x, min_y, max_x, max_y))
     }
 }
