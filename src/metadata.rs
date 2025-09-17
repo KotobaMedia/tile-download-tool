956f969097bff22621679d7505286c15768a7973
Keita Kobayashi <keita@kbys.me>
2025-09-17 11:46:05 +0900

    Add version string for download start date

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use serde::Serialize;
 
 use crate::cli::Cli;
@@ -7,14 +9,22 @@ pub struct Metadata {
     pub name: Option<String>,
     pub description: Option<String>,
     pub attribution: Option<String>,
+    pub version: Option<String>,
 }
 
 impl Metadata {
     pub fn new(cli: &Cli) -> Self {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("It can't be before 1970...")
+            .as_secs();
+        let version = format!("1.0.0.{}", now);
+
         Self {
             name: cli.name.clone(),
             description: cli.description.clone(),
             attribution: cli.attribution.clone(),
+            version: Some(version),
         }
     }
 }
